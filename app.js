e9700d7f26a8b6de90de011b290d96f3be91dda2
akifnihat <akifst@code.edu.az>
2024-07-22 01:37:38 +0400

    jsfunction

---
+++ app.js
@@ -0,0 +1,100 @@
+// Task1 
+// let n = 13;
+
+// if(n%3==0&&n%7==0){
+// console.log("3 ve 7`ye bolunur");
+// }
+// else if(n%3==0){
+// console.log("Yalniz 3 bolunur");
+// }
+// else if(n%7==0){
+//     console.log("Yalniz 7 bolunur");
+//     }
+//     else{
+//         console.log("7 ve 3 bolunmur ");
+//     }
+// Task1
+// 
+
+
+// Task2 
+// let n=16;
+// let m=26;
+// let sum=0;
+// for (let i = n; i < m; i++) {
+//     if (i%2 !==0) {
+//         sum++;
+//     }
+// }
+// console.log(sum);
+
+// Task2
+
+
+// Task3
+// let n=16;
+// let m=26;
+// let sum=0;
+// for (let i = n; i < m; i++) {
+//     if (i%2 !==0) {
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
+
+// Task3
+
+
+// Task4
+// let n=5;
+// let sum =true
+// for (let i = 2; i < n; i++) {
+//     if (n%i==0) {
+//         sum=false;
+//     }
+// }
+// if (sum) {
+//     console.log("Sade");
+// }
+// else{
+//     console.log("Murekkebdi");
+// }
+// Task4
+
+
+// Task5
+// let arr =[1,2,4,6,7,9];
+// let sum =0;
+// for (let i = 0; i < arr.length; i++) {
+//    if (arr[i]%2==0) {
+//     sum+=arr[i];
+//    }
+    
+// }
+// console.log(sum);
+// Task5
+
+// Task6
+// function totalRange(n,m) {
+// if(typeof n !=='number' || typeof m !=='number' || isNaN(n) || isNaN(m))    {
+//     return 'Her iki parametr reqem olmalidir.'
+// }
+
+// let sum = 0;
+
+// for (let i = n; i <= m; i++) {
+    
+//     sum+=i;
+// }
+
+// return sum;
+
+// }
+// let n=10;
+// let m=15;
+// let result = totalRange(n,m);
+
+// console.log(result);
+
+//Task6
+
